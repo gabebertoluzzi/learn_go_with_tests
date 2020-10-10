1afa086fae0fcbc6d8cd46fc32a2ef3311b0376a
Gabe Bertoluzzi <gabe.bertoluzzi@gmail.com>
2020-10-10 10:35:48 -0500

    Add multi-language support and switch/case logic

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const dutch = "Dutch"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const dutchHelloPrefix = "Hallo, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case dutch:
+		prefix = dutchHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,14 +12,32 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Gabe")
+		got := Hello("Gabe", "")
 		want := "Hello, Gabe"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Esmé", "French")
+		want := "Bonjour, Esmé"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Dutch", func(t *testing.T) {
+		got := Hello("Gabe", "Dutch")
+		want := "Hallo, Gabe"
 		assertCorrectMessage(t, got, want)
 	})
 }
